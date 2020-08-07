f73c4ef7ec83202d9c78736383aa8fa3da697ae6
henry <henry.byrd@claricode.com>
2020-08-06 21:20:40 -0400

    no idea what's going on

---
+++ src/lesson0/lesson0.cpp
@@ -0,0 +1,18 @@
+#include <SDL.h>
+#include <iostream>
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int, char**)
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDL Init error: " << SDL_GetError() << std::endl;
+        return 0;
+    }
+
+    SDL_Quit();
+    
+    return 0;
+}
