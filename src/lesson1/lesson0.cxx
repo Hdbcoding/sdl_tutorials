dde89b67da1ed1191f87d8cb098d900a3a7ed282
henry <henry.byrd@claricode.com>
2020-11-03 16:10:14 -0500

    Starting to put together lesson 1

---
+++ src/lesson1/lesson0.cxx
@@ -0,0 +1,37 @@
+#include <SDL.h>
+#include <iostream>
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int, char**)
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDL Init error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // create a window
+    SDL_Window *win = SDL_CreateWindow("Hello world!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr){
+        std::cout << "SDL_CreateWindow error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // create a renderer which can draw to the window
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr){
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // load bitmap image into "surface"
+
+    SDL_Quit();
+    
+    return 0;
+}
